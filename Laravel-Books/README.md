b47796d6118a68f44dfb348b40625cae3c45eb29
Leonardo Paternostro <Lapaternostro@misena.edu.co>
2021-03-04 09:46:27 -0500

    Laravel

---
+++ Laravel-Books/README.md
@@ -0,0 +1,26 @@
+# File Storage with Laravel 8
+
+This is a Laravel 8 project with laravel/ui authentication scaffolding where we go through the process of uploading files.
+
+## How to use
+
+Code for [Files with Laravel](https://www.youtube.com/playlist?list=PLYO5TOinzgThkQWRCNcNW5fPyh-RmBIwr)
+
+- Clone the repository with __git clone__
+- Copy __.env.example__ file to __.env__
+- Edit database credentials in __.env__
+- Run __composer install__
+- Run __php artisan key:generate__
+- Run __php artisan migrate__
+- Run __npm install__
+- Run __npm run dev__
+- Run __php artisan serve__ (if you want to use other port add __--port=90__)
+
+## License
+
+Feel free to use and re-use any way you want.
+
+## More
+
+- Check out my YouTube channel [Dot Coding](https://www.youtube.com/channel/UCYobBTcVkUvIqQW3sSTGarg).
+- Feel free to send me an e-mail for support [dot.bol.lp@gmail.com](mailto:dot.bol.lp@gmail.com).
---
+++ Laravel-Books/app/Http/Controllers/BookController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $books = Book::all();
+        return view('books.index', compact('books'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('books.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $book = $request->all();
+        $book['uuid'] = (string) Str::uuid();
+
+        if($request->hasFile('book_image')){
+            /**
+             * 01 documentation way
+            */
+            // $book['book_image'] = $request->file('book_image')->store('books');
+            /**
+             * 02 save just with the name
+             */
+            // $book['book_image'] = $request->file('book_image')->getClientOriginalName();
+            // $request->file('book_image')->storeAs('folder_books', $book['book_image']);
+            /**
+             * 03 save with time ahead and original name
+             */
+            $book['book_image'] = time() . '_' . $request->file('book_image')->getClientOriginalName();
+            $request->file('book_image')->storeAs('folder_books', $book['book_image']);
+            /**
+             * 04 save in a folder with the id of the user
+             */
+            // $book['book_image'] = time() . '_' . $request->file('book_image')->getClientOriginalName();
+            // $request->file('book_image')
+            //     ->storeAs('book_folder/' . auth()->id(), $book['book_image']);
+        }
+
+        Book::create($book);
+        return redirect()->route('books.index');
+    }
+
+    public function download($uuid)
+    {
+        $book = Book::where('uuid', $uuid)->firstOrFail();
+        $pathToFile = storage_path("app/public/folder_books/" . $book->book_image);
+        return response()->download($pathToFile);
+    }
+
+    /**
+     * Store option number 5
+     */
+    // public function store(Request $request)
+    // {
+    //     $book = Book::create([
+    //         'uuid' => (string) Str::orderedUuid(),
+    //         'title' => $request->title,
+    //     ]);
+    //     if($request->hasFile('book_image'))
+    //     {
+    //         $image = $request->file('book_image')->getClientOriginalName();
+    //         $request->file('book_image')
+    //             ->storeAs('subfolfer/' . $book->id, $image);
+    //         $book->update(['book_image' => $image]);
+    //     }
+    //     return redirect()->route('books.index');
+    // }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Book $book)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Book $book)
+    {
+        //
+    }
+}
